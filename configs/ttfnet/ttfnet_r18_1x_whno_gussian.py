7c14bdc7a3b339a810e8224981eb345f35f4b5c1
jnie <jnie@example.com>
2019-09-29 11:57:41 +0800

    39

--- configs/ttfnet/ttfnet_r18_1x_whno_gussian.py
+++ configs/ttfnet/ttfnet_r18_1x_whno_gussian.py
@@ -37,7 +37,8 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/raid/jing/data/dataset/coco2017/'
+# data_root = '/raid/jing/data/dataset/coco2017/'
+data_root = '/raid/jnie/data/coco2017/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
