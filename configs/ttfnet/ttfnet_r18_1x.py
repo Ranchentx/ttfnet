5cd79781b7cf113d9501302743d1321625da6a75
jnie <jnie@example.com>
2019-09-30 20:44:05 +0800

    39

--- configs/ttfnet/ttfnet_r18_1x.py
+++ configs/ttfnet/ttfnet_r18_1x.py
@@ -42,7 +42,7 @@ data_root = '/raid/jnie/dataset/coco2017/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=2,
+    imgs_per_gpu=16,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
