a0b0ec5a4bae262d85183f9c1e80460cfb0d7c64
Ranchentx <jingnie@tju.edu.cn>
2019-10-07 23:57:45 +0800

    not evaluate during training

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,4 +7,4 @@ GPUS=$2
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS  \
     --master_port=$((RANDOM + 10000)) \
-    $(dirname "$0")/train.py $CONFIG --validate --launcher pytorch ${@:3} --autoscale-lr --gpus=$GPUS
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3} --autoscale-lr --gpus=$GPUS
