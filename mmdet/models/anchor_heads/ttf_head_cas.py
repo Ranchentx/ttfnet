0d6ef65b5a93b97ca8a1dde1cd2886bea7cac6ce
jnie <jnie@example.com>
2019-10-08 11:50:44 +0800

    cas

--- mmdet/models/anchor_heads/ttf_head_cas.py
+++ mmdet/models/anchor_heads/ttf_head_cas.py
@@ -230,8 +230,8 @@ class TTFHead_cas(AnchorHead):
                    cfg,
                    rescale=False):
         batch, cat, height, width = pred_heatmap.size()
-        pred_heatmap = pred_heatmap_2.detach().sigmoid_()
-        wh = pred_wh_2.detach()
+        pred_heatmap = pred_heatmap_3.detach().sigmoid_()
+        wh = pred_wh_3.detach()
 
         # perform nms on heatmaps
         heat = simple_nms(pred_heatmap)  # used maxpool to filter the max score
